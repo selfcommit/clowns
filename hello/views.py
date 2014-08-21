4fc5f2ba3f5f240f328b6e01f5b974da3a21d018
doboyle <dan@stackoverflow.com>
2014-08-21 11:39:42 -0400

    Fixed Clown favicon

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django import http
 
 def home(request):
-    return http.HttpResponse('<center><a href=https://www.google.com/search?q=scary+clown><img src=static/clown1.jpg></a></center>')
+    return http.HttpResponse('<head><link rel="icon" href="static/favicon.ico"></head><body><center><a href=https://www.google.com/search?q=scary+clown><img src=static/clown1.jpg></a></center></body>')
