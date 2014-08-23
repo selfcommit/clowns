715c2e41174985769d6f8d1cbaf249db161ec6d9
doboyle <dan@stackoverflow.com>
2014-08-22 23:46:34 -0400

    New Home

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 from django import http
+from django.shortcuts import render
 
 def home(request):
     return http.HttpResponse('<head><link rel="icon" href="static/favicon.ico"></head><body><center><a href=https://www.google.com/search?q=scary+clown><img src=static/clown1.jpg></a></center></body>')
